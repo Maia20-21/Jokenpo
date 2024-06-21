555f00d529e391e3b1f9211a351fe9d0c9699d7d
Maia20-21 <maiabruna70@gmail.com>
2024-06-21 14:13:45 -0300

    Create jokenpo.py

---
+++ jokenpo.py
@@ -0,0 +1,44 @@
+from random import randint
+from time import sleep
+
+item = ('PEDRA', 'PAPEL', 'TESOURA')
+computador = randint(0, 2)
+
+print('Vamos jogar PEDRA, PAPEL E TESOURA')
+
+print('''
+OPÇÔES:
+[0] PEDRA
+[1] PAPEL
+[2] TESOURA
+''')
+
+jogador = int(input('Qual é a sua jogada? '))
+
+print('\nJO')
+sleep(0.7)
+print('KEN')
+sleep(0.7)
+print('PO')
+sleep(0.7)
+
+print(f'''
+O computador escolheu {item[computador]}
+O jogador escolheu {item[jogador]}
+''')
+
+if computador == jogador:
+    print('EMPATE')
+else:
+    if computador == 0 and jogador == 1:
+        print('JOGADOR vence')
+    elif computador == 1 and jogador == 0:
+        print('COMPUTADOR vence')
+    elif computador == 2 and jogador == 0:
+        print('JOGADOR vence')
+    elif computador == 0 and jogador == 2:
+        print('COMPUTADOR vence')
+    elif computador == 2 and jogador == 1:
+        print('COMPUTADOR vence')
+    else:
+        print('JOGADOR vence')
